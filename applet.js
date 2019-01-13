a065cd50f571165155c3b22c9a60e27221465623
Stefan Silviu <sylviu44@gmail.com>
2019-01-13 14:18:35 +0200

    Add button to set speed to 50%

--- applet.js
+++ applet.js
@@ -42,28 +42,42 @@ function convertRange(number, oldRange, newRange) {
   return newRange.start + normalized * (newRange.end - newRange.start);
 }
 
+function parsePwmValue(raw) {
+  return parseInt(raw.toString().trim(), 10);
+}
+
 const hwmonDirPath = '/sys/class/hwmon/hwmon2/';
-const namePath = `${hwmonDirPath}/name`;
-const pwmPath = `${hwmonDirPath}/pwm1`;
 
 class FanControlApplet extends Applet.TextApplet {
   constructor(orientation, panelHeight, instanceId) {
     super(orientation, panelHeight, instanceId);
-    this.set_applet_tooltip(`hwmon name: ${readPath(namePath).trim()}`);
-    // Make the menu
+    this.set_applet_tooltip(`hwmon name: ${readPath(`${hwmonDirPath}/name`).trim()}`);
+    // Create the menu
     this.menuManager = new PopupMenu.PopupMenuManager(this);
     this.menu = new Applet.AppletPopupMenu(this, orientation);
     this.menuManager.addMenu(this.menu);
+    // Fan speed slider
     this.slider = new PopupMenu.PopupSliderMenuItem(0);
     this.slider.connect('value-changed', (slider, value) => {
-      this.pwmValue = Math.round(convertRange(value, {start: 0, end: 1}, {start: 0, end: 255}));
+      this.pwmValue = Math.round(convertRange(value, {start: 0, end: 1}, this.pwmRange));
     });
     this.slider.connect('drag-end', () => {
       writeFile(this.pwmFile, this.pwmValue.toString());
     });
     this.menu.addMenuItem(this.slider);
+    // Set to 50% button
+    this.setTo50 = new PopupMenu.PopupIconMenuItem('Set to 50%', 'object-flip-horizontal', 'normal');
+    this.setTo50.connect('activate', () => {
+      this.pwmValue = Math.round(convertRange(50.0, {start: 0, end: 100}, this.pwmRange)) + 4; // FIXME: offset
+      writeFile(this.pwmFile, this.pwmValue.toString());
+    });
+    this.menu.addMenuItem(this.setTo50);
     // Get pwm file and update views
-    this.pwmFile = Gio.File.new_for_path(pwmPath);
+    this.pwmFile = Gio.File.new_for_path(`${hwmonDirPath}/pwm1`);
+    this.pwmRange = {
+      start: parsePwmValue(readPath(`${hwmonDirPath}/pwm1_min`)),
+      end: parsePwmValue(readPath(`${hwmonDirPath}/pwm1_max`))
+    };
     this.update();
     // Monitor pwm changes
     this.monitor = this.pwmFile.monitor(0, null);
@@ -76,9 +90,9 @@ class FanControlApplet extends Applet.TextApplet {
   }
 
   update() {
-    this.pwmValue = parseInt(readFile(this.pwmFile).toString().trim(), 10);
-    this.slider.setValue(convertRange(this.pwmValue, {start: 0, end: 255}, {start: 0, end: 1}));
-    const percent = Math.round(convertRange(this.pwmValue, {start: 0, end: 255}, {start: 0, end: 100}));
+    this.pwmValue = parsePwmValue(readFile(this.pwmFile));
+    this.slider.setValue(convertRange(this.pwmValue, this.pwmRange, {start: 0, end: 1}));
+    const percent = Math.round(convertRange(this.pwmValue, this.pwmRange, {start: 0, end: 100}));
     this.set_applet_label(`GPU Fans: ${percent}%`);
   }
 }
